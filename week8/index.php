dffb650a471298d5c25b4396d2562f4a68023db7
trperez <trperez@student.apc.edu.ph>
2024-01-23 13:00:44 +0800

    added file

---
+++ week8/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!"
+?>
+
+<?php
+ECHO "Hello World!" <br>;
+echo "Hello World!" <br>;
+ECHO "Hello World!" <br>;
+?>
+
+</body>
+</html>
