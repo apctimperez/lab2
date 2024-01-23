352c9381546e4453fc55016240996cdb6b9b3820
trperez <trperez@student.apc.edu.ph>
2024-01-23 14:02:18 +0800

    added file

--- week8/index.php
+++ week8/index.php
@@ -5,17 +5,14 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tim Perez - Personal Portfolio</title>
+  <title>Timothy Perez - Personal Portfolio</title>
 
   <!--
     - favicon
   -->
   <link rel="shortcut icon" href="tim1.jpg" type="image/x-icon">
 
-  <!--
-    - custom css link
-  -->
-  <link rel="stylesheet" href="./assets/css/style.css">
+
 
   <!--
     - google font link
