d76906fd57caa19e79704004aedac6cb079394dd
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2024-01-30 21:58:27 +0800

    edited file php

--- week8/forms.php
+++ week8/forms.php
@@ -2141,29 +2141,35 @@ function test_input($data) {
   return $data;
 }
 ?>
-<form>
-<h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-<label>Name: </label><input type="text" name="name" value="<?php echo $name;?>">
-<span class="error">* <?php echo $nameErr;?></span>
-<br><br>
- <label> E-mail: </label><input type="text" name="email" value="<?php echo $email;?>">
-<span class="error">* <?php echo $emailErr;?></span>
-<br><br>
-<label>  Website: </label><input type="text" name="website" value="<?php echo $website;?>">
-<span class="error"><?php echo $websiteErr;?></span>
-<br><br>
-<label>  Comment: </label><textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-<br><br>
-<label>  Gender: </label>
-<input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-<input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-<input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-<span class="error">* <?php echo $genderErr;?></span>
-<br><br>
-<input type="submit" name="submit" value="Submit">  
-</form>
+<div class="container">
+  <h2>PHP Form Validation Example</h2>
+  <p><span class="error">* required field</span></p>
+
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name" value="<?php echo $name;?>">
+    <span class="error">* <?php echo $nameErr;?></span>
+
+    <label for="email">E-mail:</label>
+    <input type="text" name="email" id="email" value="<?php echo $email;?>">
+    <span class="error">* <?php echo $emailErr;?></span>
+
+    <label for="website">Website:</label>
+    <input type="text" name="website" id="website" value="<?php echo $website;?>">
+    <span class="error"><?php echo $websiteErr;?></span>
+
+    <label for="comment">Comment:</label>
+    <textarea name="comment" id="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+
+    <label>Gender:</label>
+    <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+    <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+    <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+    <span class="error">* <?php echo $genderErr;?></span>
+
+    <input type="submit" name="submit" value="Submit">  
+  </form>
+
 <?php
 echo "<h2>Your Input:</h2>";
 echo $name;
