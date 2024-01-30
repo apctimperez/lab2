691748970a652d299e4aebbdab3e35cf5123c77d
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2024-01-30 21:55:54 +0800

    edited php

--- week8/forms.php
+++ week8/forms.php
@@ -1907,6 +1907,7 @@ textarea.form-input::-webkit-resizer { display: none; }
     label {
       display: block;
       margin-bottom: 5px;
+      color: white;
     }
     input, textarea {
       width: 100%;
@@ -2144,18 +2145,18 @@ function test_input($data) {
 <h2>PHP Form Validation Example</h2>
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
- <p> Name: </p><input type="text" name="name" value="<?php echo $name;?>">
+<label>Name: </label><input type="text" name="name" value="<?php echo $name;?>">
 <span class="error">* <?php echo $nameErr;?></span>
 <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+ <label> E-mail: </label><input type="text" name="email" value="<?php echo $email;?>">
 <span class="error">* <?php echo $emailErr;?></span>
 <br><br>
-  Website: <input type="text" name="website" value="<?php echo $website;?>">
+<label>  Website: </label><input type="text" name="website" value="<?php echo $website;?>">
 <span class="error"><?php echo $websiteErr;?></span>
 <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+<label>  Comment: </label><textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
 <br><br>
-  Gender:
+<label>  Gender: </label>
 <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
 <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
 <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
