0ec174d8dcb2ff3bb77f623070d680362ca3cfaa
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2024-02-06 13:27:25 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -19,7 +19,7 @@ if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id , name, email, website, comment, gender FROM MyGuests";
+$sql = "SELECT id , name, email, website, comment, gender FROM trperez_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0){
