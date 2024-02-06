51b1cc7d89049b557954e46a3a9bd3b8db357b23
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2024-02-06 12:26:38 +0800

    Update forms.php

--- week8/forms.php
+++ week8/forms.php
@@ -2223,18 +2223,7 @@ echo $comment;
 echo "<br>";
 echo $gender;
 ?>
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
+
 
 
 <?php
